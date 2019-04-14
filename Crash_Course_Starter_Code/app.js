46c3011e5540b92682ed70df2321bf60695da52e
kagabuisa <kagabuisa@gmail.com>
2019-04-14 20:12:50 +0300

    first commit

---
+++ Crash_Course_Starter_Code/app.js
@@ -0,0 +1,9 @@
+// Write your JS in here
+pics = [
+	"imgs/kitty_bed.jpg",
+	"imgs/kitty_basket.jpg", 
+	"imgs/kitty_laptop.jpg",
+	"imgs/kitty_door.jpg",
+	"imgs/kitty_sink.jpg",
+	"imgs/kitty_wall.jpg"
+]
